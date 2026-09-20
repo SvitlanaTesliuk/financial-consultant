c9842fbb63f2f3998663ed2d755fcd6f12e436a7
SvitlanaTesliuk <svitlanateslyuk277@gmail.com>
2026-09-20 21:25:06 +0200

    fix gmail smtp configuration

--- server/server.js
+++ server/server.js
@@ -27,11 +27,10 @@ app.use(
 app.use(express.json())
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
-  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
