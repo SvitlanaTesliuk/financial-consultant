b1bd376804b37ba3de99aa096212a77b9334244d
SvitlanaTesliuk <svitlanateslyuk277@gmail.com>
2026-09-20 21:13:42 +0200

    fix cors for vercel deployments

--- server/server.js
+++ server/server.js
@@ -10,7 +10,17 @@ app.disable('x-powered-by')
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL ||'http://localhost:5173',
+    origin: (origin, callback) => {
+      if (
+        !origin ||
+        origin.startsWith('https://financial-consultant-') ||
+        origin === 'http://localhost:5173'
+      ) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
   }),
 )
 
