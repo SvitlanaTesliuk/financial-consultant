8b9356bf64a6c2563fb179c68b9fb0148415bb9b
SvitlanaTesliuk <svitlanateslyuk277@gmail.com>
2026-09-20 18:33:01 +0200

    commit real api

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -68,7 +68,7 @@ const Contact = () => {
 
     try {
       const response = await fetch(
-        'http://localhost:5000/api/contact',
+        'https://financial-consultant.onrender.com/api/contact',
         {
           method: 'POST',
           headers: {
